da89be51b47449f5cf6ebf6620360fd38728cf0d
Eduardo Bianco <edubiapin@gmail.com>
2023-08-23 10:51:14 -0300

    Update seller and custom service exception

--- WebSalesMvc/Controllers/SellersController.cs
+++ WebSalesMvc/Controllers/SellersController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using WebSalesMvc.Models;
 using WebSalesMvc.Services;
 using WebSalesMvc.Models.ViewModels;
+using WebSalesMvc.Services.Exceptions;
 
 namespace WebSalesMvc.Controllers
 {
@@ -46,7 +47,7 @@ namespace WebSalesMvc.Controllers
             {
                 return NotFound();
             }
-            var obj = _sellerService.FinById(id.Value);
+            var obj = _sellerService.FindById(id.Value);
             if (obj == null)
             {
                 return NotFound();
@@ -68,13 +69,52 @@ namespace WebSalesMvc.Controllers
             {
                 return NotFound();
             }
-            var obj = _sellerService.FinById(id.Value);
+            var obj = _sellerService.FindById(id.Value);
             if (obj == null)
             {
                 return NotFound();
             }
             return View(obj);
 
+        }
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var obj = _sellerService.FindById(id.Value);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            List<Department> departments = _departmentService.FindAll();
+            SellerFormViewModel viewModel = new SellerFormViewModel { Seller = obj, Departments = departments };
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Seller seller)
+        {
+            if (id != seller.Id)
+            {
+                return BadRequest();
+            }
+            try
+            {
+                _sellerService.Update(seller);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (NotFoundException)
+            {
+                return NotFound();
+            }
+            catch (DbConcurrencyException)
+            {
+                return BadRequest();
+            }
         }
     }
 }
